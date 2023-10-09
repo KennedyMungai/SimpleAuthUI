8852c80b4913fa6b5868f59abfd60db2a819731b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-09 15:16:15 +0300

    Gave the Link some button stylings

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import { buttonVariants } from "../ui/button";
 
 type Props = {};
 
@@ -8,7 +9,12 @@ const NavBar = (props: Props) => {
     <nav className="bg-zinc-100 dark:bg-zinc-900 py-4 border-b border-s-zinc-200 dark:border-s-zinc-800 fixed w-full z-10 top-0">
       <div className="container flex items-center justify-between">
         <Link href={"/"}>🌱 Some App</Link>
-        <Link href={"/sign-in"}>Sign In</Link>
+        <Link
+          href={"/sign-in"}
+          className={buttonVariants({ variant: "outline" })}
+        >
+          Sign In
+        </Link>
       </div>
     </nav>
   );
