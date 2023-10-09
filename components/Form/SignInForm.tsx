42d18793d58ce2776df63279502254d1ae4ddc26
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-09 15:48:27 +0300

    A template for the DigninForm

--- components/Form/SignInForm.tsx
+++ components/Form/SignInForm.tsx
@@ -1,8 +1,59 @@
 "use client";
+import { useForm } from "react-hook-form";
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+} from "../ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { Input } from "../ui/input";
+
 type Props = {};
 
+const FormSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
 const SignInForm = (props: Props) => {
-  return <div>SignInForm</div>;
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const onSubmit = () => {
+    console.log("form submitted");
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default SignInForm;
