8d5b05e668b9deae5f1c8f26eff93cab088bb0fe
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-09 15:13:24 +0300

    Made some adjustments for the NavBar component

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,11 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
 const NavBar = (props: Props) => {
   return (
-    <nav className="bg-zinc-100 dark:bg-zinc-900 py-2 border-b border-s-zinc-200 dark:border-s-zinc-800 fixed w-full z-10 top-0">
-      <div className="container">Login</div>
+    <nav className="bg-zinc-100 dark:bg-zinc-900 py-4 border-b border-s-zinc-200 dark:border-s-zinc-800 fixed w-full z-10 top-0">
+      <div className="container flex items-center justify-between">
+        <Link href={"/"}>🌱 Some App</Link>
+        <Link href={"/sign-in"}>Sign In</Link>
+      </div>
     </nav>
   );
 };
