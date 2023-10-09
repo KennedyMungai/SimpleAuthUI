7bceb2377eb4ab57dfe66bee93349aa5e404f495
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-09 16:31:07 +0300

    Created the sign up form

--- components/Form/SignUpForm.tsx
+++ components/Form/SignUpForm.tsx
@@ -1,8 +1,95 @@
 "use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import Link from "next/link";
+
 type Props = {};
 
+const FormSchema = z.object({
+  email: z
+    .string()
+    .email("Invalid Email")
+    .min(5, "At least 5 characters are required for the email"),
+  password: z
+    .string()
+    .min(8, "The password should not be less than 8 characters"),
+});
+
 const SignUpForm = (props: Props) => {
-  return <div>SignUpForm</div>;
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const onSubmit = (values: z.infer<typeof FormSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+        <div className="space-y-2">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Email" {...field} className="bg-white" />
+                </FormControl>
+                <FormDescription>
+                  Enter your email address to continue
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Password"
+                    {...field}
+                    type="password"
+                    className="bg-white"
+                  />
+                </FormControl>
+                <FormDescription>
+                  Enter your password to continue
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="w-full mt-2">
+            Sign In
+          </Button>
+          <p className="text-center">Or</p>
+          <Link href="/sign-up">
+            <Button type="submit" className="w-full mt-2" variant={"outline"}>
+              Sign Up
+            </Button>
+          </Link>
+        </div>
+      </form>
+    </Form>
+  );
 };
 
 export default SignUpForm;
