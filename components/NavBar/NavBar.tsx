ad6ee8d21384120fb05ac68e79d3b877155e788e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-09 15:18:44 +0300

    Made some sosmetic updates

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -8,7 +8,10 @@ const NavBar = (props: Props) => {
   return (
     <nav className="bg-zinc-100 dark:bg-zinc-900 py-4 border-b border-s-zinc-200 dark:border-s-zinc-800 fixed w-full z-10 top-0">
       <div className="container flex items-center justify-between">
-        <Link href={"/"}>🌱 Some App</Link>
+        <Link href={"/"}>
+          {" "}
+          <h1 className="text-2xl">🌱</h1>
+        </Link>
         <Link
           href={"/sign-in"}
           className={buttonVariants({ variant: "outline" })}
