652d4a39ece87490f22e4eb419d3bd63379f5f0f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-09 15:10:33 +0300

    Made some changes

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const NavBar = (props: Props) => {
   return (
     <nav className="bg-zinc-100 dark:bg-zinc-900 py-2 border-b border-s-zinc-200 dark:border-s-zinc-800 fixed w-full z-10 top-0">
-      <div className="">Login</div>
+      <div className="container">Login</div>
     </nav>
   );
 };
