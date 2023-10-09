b2044d43f27ef83815878f2636af67aa57b79f3a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-09 16:28:46 +0300

    Rendered the SignUpForm component on the page

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,14 @@
+import SignUpForm from "@/components/Form/SignUpForm";
 import React from "react";
 
 type Props = {};
 
 const SignUpPage = (props: Props) => {
-  return <div>SignUpPage</div>;
+  return (
+    <div>
+      <SignUpForm />
+    </div>
+  );
 };
 
 export default SignUpPage;
